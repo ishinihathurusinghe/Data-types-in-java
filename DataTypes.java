4c8e4d135c88524d8f6a7d22aa20c68c6569d7d7
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-03-07 12:21:48 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,35 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        boolean myBool = true;
+        System.out.println(myBool);  // Output: true
+
+
+        byte myByte = 127;
+        System.out.println(myByte);  // Output: 127
+
+
+        short myShort = 32767;
+        System.out.println(myShort);  // Output: 32767
+
+
+        int myInt = 2147483647;
+        System.out.println(myInt);  // Output: 2147483647
+
+
+        long myLong = 9223372036854775807L;
+        System.out.println(myLong);  // Output: 9223372036854775807
+
+
+        float myFloat = 3.14f;
+        System.out.println(myFloat);  // Output: 3.14
+
+
+        double myDouble = 2.71828;
+        System.out.println(myDouble);  // Output: 2.71828
+
+
+        char myChar = 'A';
+        System.out.println(myChar);  // Output: A
+    }
+}
